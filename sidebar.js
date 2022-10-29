2f2493cc4c10fc2a2a314502c0d7f364c56ea45d
KristinBArnardottir <kristinbjorga@gmail.com>
2022-10-29 16:25:15 +0100

    First two Prompts uploaded
    Uploaded first two prompts. Moved sidebar to sidebar.js

---
+++ sidebar.js
@@ -0,0 +1,10 @@
+document.write('\
+\
+    <ul>\
+      <li><a href="index.html">Art Prompts</a></li>\
+      <li><a href="prompt1.html">Pick</a></li>\
+      <li><a href="prompt2.html">Fuzzy</a></li>\
+      <li><a href="prompt3.html">Rocket</a></li>\
+    </ul>\
+\
+') 
